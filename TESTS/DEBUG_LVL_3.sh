b2a9c6fc44030e0fc2753b8cdd20097b03ae9b6d
ag1455 <a.g.prosat@gmail.com>
2020-01-19 12:12:14 +0300

    Minor logging fixes

--- TESTS/DEBUG_LVL_3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd ..
-ENIGMA_DEBUG_LVL=3 /usr/local/e2/bin/enigma2.sh > LOG.ENIGMA 2>&1
--- TESTS/DEBUG_LVL_4.sh
+++ TESTS/DEBUG_LVL_4.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd ..
-ENIGMA_DEBUG_LVL=4 /usr/local/e2/bin/enigma2.sh > LOG.ENIGMA 2>&1
+ENIGMA_DEBUG_LVL=4 /usr/local/e2/bin/enigma2.sh
