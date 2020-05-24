06fe5d7bd6695c607c3354d010330bd435f4337a
ag1455 <a.g.prosat@gmail.com>
2020-05-24 18:59:31 +0300

    Update plugin.

--- 4_build_plugins.sh
+++ 4_build_plugins.sh
@@ -398,7 +398,7 @@ if [ -d plugins ]; then
 		cp -r pre/icons plugins/e2openplugin/e2iplayer/IPTVPlayer
 		cp patches/E2IPlayer.patch plugins/e2openplugin/e2iplayer
 		cd plugins/e2openplugin/e2iplayer
-		git checkout --detach b0baea22
+		git checkout --detach 522dd993
 		patch -p1 < E2IPlayer.patch
 		rm -rf IPTVPlayer/locale/uk
 		rm -f IPTVPlayer/locale/ru/LC_MESSAGES/.gitkeep
