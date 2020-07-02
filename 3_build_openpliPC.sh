8a898ccbd203cc57865e94dc840e8bf17b8b0322
ag1455 <a.g.prosat@gmail.com>
2020-07-02 23:52:58 +0300

    Enigma2 - 20.07.02.

--- 3_build_openpliPC.sh
+++ 3_build_openpliPC.sh
@@ -136,7 +136,7 @@ rpl "//#define XINE_TEXTDOMAIN" "#define XINE_TEXTDOMAIN" /usr/include/xine/xine
 
 git clone https://github.com/OpenPLi/$PKG.git
 cd $PKG
-git reset --hard 6415c212
+git reset --hard 27482ef2
 cd ..
 
 cp -fv $PKG/data/display/skin_display_default.xml $PKG/data/display/skin_display.xml
@@ -155,10 +155,10 @@ if [ "$release" = "14.04" ]; then
 	echo "********************************************************"
 	echo ""
 	export CXX=/usr/bin/g++-6
-	cp patches/patch-6415c212-to-PC.patch $PKG
+	cp patches/patch-27482ef2-to-PC.patch $PKG
 	cp patches/ubuntu-14.04.patch $PKG
 	cd $PKG
-	patch -p1 < patch-6415c212-to-PC.patch
+	patch -p1 < patch-27482ef2-to-PC.patch
 	patch -p1 < ubuntu-14.04.patch
 elif [ "$release" = "16.04" ]; then
 	echo ""
@@ -168,9 +168,9 @@ elif [ "$release" = "16.04" ]; then
 	echo "********************************************************"
 	echo ""
 	export CXX=/usr/bin/g++-7
-	cp patches/patch-6415c212-to-PC.patch $PKG
+	cp patches/patch-27482ef2-to-PC.patch $PKG
 	cd $PKG
-	patch -p1 < patch-6415c212-to-PC.patch
+	patch -p1 < patch-27482ef2-to-PC.patch
 elif [ "$release" = "18.04" ]; then
 	echo ""
 	echo "********************************************************"
@@ -178,9 +178,9 @@ elif [ "$release" = "18.04" ]; then
 	echo "                  *** USED g++-7 ***"
 	echo "********************************************************"
 	export CXX=/usr/bin/g++-7
-	cp patches/patch-6415c212-to-PC-sigc2.patch $PKG
+	cp patches/patch-27482ef2-to-PC-sigc2.patch $PKG
 	cd $PKG
-	patch -p1 < patch-6415c212-to-PC-sigc2.patch
+	patch -p1 < patch-27482ef2-to-PC-sigc2.patch
 elif [ "$release" = "19.04" ]; then
 	echo ""
 	echo "********************************************************"
@@ -189,9 +189,9 @@ elif [ "$release" = "19.04" ]; then
 	echo "********************************************************"
 	echo ""
 	export CXX=/usr/bin/g++-8
-	cp patches/patch-6415c212-to-PC-sigc2.patch $PKG
+	cp patches/patch-27482ef2-to-PC-sigc2.patch $PKG
 	cd $PKG
-	patch -p1 < patch-6415c212-to-PC-sigc2.patch
+	patch -p1 < patch-27482ef2-to-PC-sigc2.patch
 elif [ "$release" = "19.10" ]; then
 	echo ""
 	echo "********************************************************"
@@ -200,9 +200,9 @@ elif [ "$release" = "19.10" ]; then
 	echo "********************************************************"
 	echo ""
 	export CXX=/usr/bin/g++-9
-	cp patches/patch-6415c212-to-PC-sigc2.patch $PKG
+	cp patches/patch-27482ef2-to-PC-sigc2.patch $PKG
 	cd $PKG
-	patch -p1 < patch-6415c212-to-PC-sigc2.patch
+	patch -p1 < patch-27482ef2-to-PC-sigc2.patch
 elif [ "$release" = "20.04" ]; then
 	echo ""
 	echo "********************************************************"
@@ -211,10 +211,10 @@ elif [ "$release" = "20.04" ]; then
 	echo "********************************************************"
 	echo ""
 	export CXX=/usr/bin/g++-9
-	cp patches/patch-6415c212-to-PC-sigc2.patch $PKG
+	cp patches/patch-27482ef2-to-PC-sigc2.patch $PKG
 	cp patches/20_04_Makefile.am.patch $PKG
 	cd $PKG
-	patch -p1 < patch-6415c212-to-PC-sigc2.patch
+	patch -p1 < patch-27482ef2-to-PC-sigc2.patch
 	patch -p1 < 20_04_Makefile.am.patch
 fi
 
