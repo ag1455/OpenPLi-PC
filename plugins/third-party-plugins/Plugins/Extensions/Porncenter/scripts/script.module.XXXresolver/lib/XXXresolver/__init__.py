a5d5ebfb9754a6beca560d14ae1254fc21aa9b14
ag1455 <a.g.prosat@gmail.com>
2020-11-20 18:07:25 +0300

    Remove unused plugins.

--- plugins/third-party-plugins/Plugins/Extensions/Porncenter/scripts/script.module.XXXresolver/lib/XXXresolver/__init__.py
+++
@@ -1,6 +0,0 @@
-sources = ["empflix", "tnaflix", "pornhub", "motherless", "shemaletubevideos", "xnxx", "luxuretv", "hotgoo", "heavy-r", "xhamster", "spicytranny", "deviantclip", "xvideos", "txxx", "befuck", "pornxs", "sheshaft", "ashemaletube", "sunporno", "jizzbunker", "pornoxo", "youporn", "vporn"]
-
-
-
-
-
