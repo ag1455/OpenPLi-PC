29bc2b2412f294a123a2dff1a1130af84aa5e335
ag1455 <a.g.prosat@gmail.com>
2021-02-25 11:51:27 +0300

    Update e2iplayer version.

--- 5_build_plugins.sh
+++ 5_build_plugins.sh
@@ -451,7 +451,7 @@ if [ -d plugins ]; then
 		cp -rv pre/icons plugins/e2openplugin/e2iplayer/IPTVPlayer
 		cp -fv patches/E2IPlayer.patch plugins/e2openplugin/e2iplayer
 		cd plugins/e2openplugin/e2iplayer
-		git checkout --detach e0f0d651
+		git checkout --detach 4f526d46
 		patch -p1 < E2IPlayer.patch
 		rm -f IPTVPlayer/locale/ru/LC_MESSAGES/.gitkeep
 		if [ "$release" = "20.04" ]; then
