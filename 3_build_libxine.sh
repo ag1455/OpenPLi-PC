6ba8449fe9f8d9c8fa4765c003500b768cc664b8
ag1455 <a.g.prosat@gmail.com>
2021-04-08 11:59:06 +0300

    Update libxine2 to version 14839.

--- 3_build_libxine.sh
+++ 3_build_libxine.sh
@@ -20,13 +20,13 @@ else
 fi
 
 # This is hg 1.2.10
-hg clone -r14835 http://hg.code.sf.net/p/xine/xine-lib-1.2 $PKG
+hg clone -r14839 http://hg.code.sf.net/p/xine/xine-lib-1.2 $PKG
 
 if [ -d "$PKG" ]; then
 	echo "-----------------------------------------"
-	echo "      head now on 14835:7ae23987a9b5"
+	echo "      head now on 14839:84e05338d7e3"
 	echo "-----------------------------------------"
-	cp -fv patches/xine-lib-1.2-14835:7ae23987a9b5.patch $PKG
+	cp -fv patches/xine-lib-1.2-14839:84e05338d7e3.patch $PKG
 else
 	echo "-----------------------------------------"
 	echo "        CHECK INTERNET CONNECTION!"
@@ -34,7 +34,7 @@ else
 fi
 
 cd $PKG
-patch -p1 < xine-lib-1.2-14835:7ae23987a9b5.patch
+patch -p1 < xine-lib-1.2-14839:84e05338d7e3.patch
 echo "-----------------------------------------"
 echo "       patch for xine-lib applied"
 echo "-----------------------------------------"
