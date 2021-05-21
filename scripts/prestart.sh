cee0340490103a5fcc7f24a734d3ca6b2dea2b61
ag1455 <a.g.prosat@gmail.com>
2021-05-21 20:19:45 +0300

    Change for two single or dual dvb cards.

--- scripts/prestart.sh
+++ scripts/prestart.sh
@@ -3,24 +3,38 @@
 # This is only for LCD!
 # ln -s /dev/fb0 /dev/fb1
 
-#if [ -d /sys/class/dvb/dvb0.frontend1 ]; then
-#	rm $DVB_DEV/net1 $DVB_DEV/frontend1 $DVB_DEV/demux1 $DVB_DEV/dvr1 $DVB_DEV/net1
-#fi
-
-DVB_DEV="/dev/dvb/adapter0"
+DVB_DEV0="/dev/dvb/adapter0"
+DVB_DEV1="/dev/dvb/adapter1"
 LOG="/var/log/oscam/wait.log"
+
+# Single with 2 frontends
+if [ -d /sys/class/dvb/dvb0.frontend0 ]; then
+	ln -s $DVB_DEV0/frontend0 $DVB_DEV1/frontend1
+	ln -s $DVB_DEV0/demux0 $DVB_DEV1/demux1
+	ln -s $DVB_DEV0/dvr0 $DVB_DEV1/dvr1
+	ln -s $DVB_DEV0/net0 $DVB_DEV1/net1
+	ln -s $DVB_DEV0/ca0 $DVB_DEV1/ca1
+fi
+
+# Dual or 2 single with 1 frontend
+if [ -d /sys/class/dvb/dvb1.frontend0 ]; then
+	rm -f $DVB_DEV0/frontend1 $DVB_DEV0/demux1 $DVB_DEV0/dvr1 $DVB_DEV0/net1 $DVB_DEV0/ca1
+	ln -s $DVB_DEV1/frontend0 $DVB_DEV1/frontend1
+	ln -s $DVB_DEV1/demux0 $DVB_DEV1/demux1
+	ln -s $DVB_DEV1/dvr0 $DVB_DEV1/dvr1
+	ln -s $DVB_DEV1/net0 $DVB_DEV1/net1
+	ln -s $DVB_DEV1/ca0 $DVB_DEV1/ca1
+fi
 
 if [ ! -f $LOG ]; then
 	touch "$LOG"
 fi
 
 while [ 1 != 0 ]; do # The script will run in a loop until frontend0 appears.
-	if [ $(ls $DVB_DEV | grep -w frontend0) ]; then
-		ln -s $DVB_DEV/demux0 $DVB_DEV/demux1
-		ln -s $DVB_DEV/dvr0 $DVB_DEV/dvr1
+	if [ $(ls $DVB_DEV0 | grep -w frontend0) ]; then
 		modprobe -v dvbsoftwareca
-		chmod 660 $DVB_DEV/ca0
-		chown root:video $DVB_DEV/ca0
+		chmod 660 $DVB_DEV0/ca0
+		chown root:video $DVB_DEV0/ca0
 		sleep 10
 		softcam start
 #		echo " * Oscam started" >> $LOG
