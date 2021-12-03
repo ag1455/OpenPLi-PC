e999f1bcc30129f3c55ef47ab9c216876290af92
ag1455 <a.g.prosat@gmail.com>
2021-12-03 15:11:32 +0300

    Try to fix: oscam doesn't always start correctly.

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -13,12 +13,14 @@ LOG="/var/log/oscam/wait.log"
 rm -f $HDD/timeshift/* # Case broken pause.
 mv -f $HDD/enigma2_crash_*.log /tmp # Case hangs up.
 rm -f /tmp/ENIGMA_FIFO.sc
+sleep 2
 
 if [ $(ls $DVB_DEV0 | grep -w ca0) ]; then
-	if [[ -n `pgrep oscam` ]]; then
+	if [ $(pgrep -n oscam) ]; then
 		echo " * Oscam already started!"
 	else
 		softcam start
+#		softcam force-reload
 		echo " * Oscam started" >> $LOG
 	fi
 fi
@@ -26,7 +28,7 @@ fi
 while [ 1 != 0 ]; do
 	# Start  enigma2.
 	if [ $(ps -A | grep -c enigma2) -eq 0 ]; then
-		sleep 5 # If you want to autostart from boot.
+		sleep 3 # If you want to autostart from boot.
 		# Case screensaver is enabled.
 		xterm +ah -bg black -geometry 1x1 -e $PREFIX/enigma2.sh &
 		xset -dpms
